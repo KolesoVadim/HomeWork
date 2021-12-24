2661a48a676a8a1056f8c21504144f58706498f6
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2021-12-24 18:45:24 +0300

    Task04 done

---
+++ Task04/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать последнюю цифру трёхзначного числа.
+Console.Write("Ведите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int lastN = number % 10;
+Console.WriteLine("Последняя цифра трёхзначного числа: " + lastN);
