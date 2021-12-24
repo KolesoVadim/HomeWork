d1635a26558c85c20c514796c2e31e269795ec60
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2021-12-24 18:56:17 +0300

    Task05 done

---
+++ Task05/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа.
+Console.Write("Ведите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int second = (number / 10 % 10);
+Console.WriteLine("Вторая цифра трёхзначного числа: " + second);
