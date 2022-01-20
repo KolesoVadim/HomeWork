de045dc31fc776a9a951a56839ff0ef4e189b6b3
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-20 19:55:13 +0300

    Task06 added

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int max = new Random().Next(10, 100);
+Console.WriteLine(max);
+int numberA = max / 10;
+int numberB = max % 10;
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else 
+{
+    Console.WriteLine(numberB);
+}
