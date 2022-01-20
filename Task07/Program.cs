950fb2c2849285935523994dbed1ae421941e293
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-20 20:21:12 +0300

    Task07 added

---
+++ Task07/Program.cs
@@ -0,0 +1,6 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Трехзначное число: " + number);
+int hundred = number / 100;
+int unit = number % 10; 
+Console.WriteLine("" + hundred + unit);
